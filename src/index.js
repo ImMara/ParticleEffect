a683b4c759887c61761aa3282258717d673a4447
Mara <alaneraerts@live.be>
2021-03-05 21:01:57 +0100

    loader

---
+++ src/index.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Model from './model'
+
+/*------------------------------
+Renderer
+------------------------------*/
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
+});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+/*------------------------------
+Scene & Camera
+------------------------------*/
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 
+  50, 
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.z = 5;
+camera.position.y = 1;
+
+
+/*------------------------------
+Mesh
+------------------------------*/
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial( { 
+  color: 0x00ff00,
+} );
+const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+
+/*------------------------------
+OrbitControls
+------------------------------*/
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+/*------------------------------
+Helpers
+------------------------------*/
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+/*------------------------------
+Models
+------------------------------*/
+
+const Skull = new Model({
+  name:'skull',
+  file:'./models/skull.glb',
+  scene: scene,
+  placeOnLoad: true
+})
+
+const Horse = new Model({
+  name:'horse',
+  file:'./models/Horse.glb',
+  scene: scene,
+  placeOnLoad: false
+})
+/*------------------------------
+Controllers
+------------------------------*/
+
+const buttons = document.querySelectorAll('.button')
+buttons[0].addEventListener('click',()=>{
+  Skull.add()
+  Horse.remove()
+})
+buttons[1].addEventListener('click',()=>{
+  Skull.remove()
+  Horse.add()
+})
+
+
+/*------------------------------
+Loop
+------------------------------*/
+const animate = function () {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+};
+animate();
+
+
+/*------------------------------
+Resize
+------------------------------*/
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize, false );
