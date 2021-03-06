2ebca17c9bfb9070cb992b720a71c07ab78d56e8
Mara <alaneraerts@live.be>
2021-03-06 13:00:09 +0100

    particle animations v2

--- src/model.js
+++ src/model.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 import {MeshSurfaceSampler} from "three/examples/jsm/math/MeshSurfaceSampler";
 import vertex from './shader/vertexShader.glsl';
 import fragment from './shader/fragmentShader.glsl';
+
 
 class Model {
     constructor(obj){
@@ -62,7 +64,8 @@ class Model {
                 uniforms:{
                     uColor1:{value : new THREE.Color(this.color1)},
                     uColor2:{value: new THREE.Color(this.color2)},
-                    uTime:{ value:0 }
+                    uTime:{ value:0 },
+                    uScale:{value : 0}
                 },
                 vertexShader:vertex,
                 fragmentShader:fragment,
@@ -123,10 +126,24 @@ class Model {
     add(){
         this.scene.add(this.particles)
         this.isActive = true
+
+        gsap.to(this.particlesMaterial.uniforms.uScale,{
+            value:1,
+            duration:.8,
+            delay:.3,
+            ease:"power3.out"
+        })
     }
     remove(){
-        this.scene.remove(this.particles)
-        this.isActive = false
+        gsap.to(this.particlesMaterial.uniforms.uScale,{
+            value:0,
+            duration:.8,
+            ease:"power3.out",
+            onComplete:()=>{
+                this.scene.remove(this.particles)
+                this.isActive = false
+            }
+        })
     }
 }
 export default Model;
--- src/shader/vertexShader.glsl
+++ src/shader/vertexShader.glsl
@@ -6,13 +6,23 @@ varying vec3 vPosition;
 
 uniform float uTime;
 
+uniform float uScale;
+
 void main() {
     vPosition= position;
 
+    float time = uTime * 4.;
+
     vec3 pos = position;
-    pos.x += sin(uTime * aRandom.x) * 0.01;
-    pos.y += cos(uTime * aRandom.y) * 0.01;
-    pos.z += cos(uTime * aRandom.z) * 0.01;
+    pos.x += sin(time * aRandom.x) * 0.01;
+    pos.y += cos(time * aRandom.y) * 0.01;
+    pos.z += cos(time * aRandom.z) * 0.01;
+
+    pos.x *= (uScale + sin(pos.y * 4. + time)*(1.- uScale));
+    pos.y *= (uScale + sin(pos.z * 4. + time)*(1.- uScale));
+    pos.z *= (uScale + sin(pos.x * 4. + time)*(1.- uScale));
+
+    pos *= uScale;
 
     vec4 mvPosition = modelViewMatrix * vec4( pos, 1.0 );
     gl_Position = projectionMatrix * mvPosition;
