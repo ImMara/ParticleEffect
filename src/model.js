0c08ebaf360962a09399af65a420c9025e11593e
Mara <alaneraerts@live.be>
2021-03-06 10:20:57 +0100

    particles

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
+import {MeshSurfaceSampler} from "three/examples/jsm/math/MeshSurfaceSampler";
 
 class Model {
     constructor(obj){
@@ -19,16 +20,70 @@ class Model {
 
     init (){
         this.loader.load(this.file,(response)=>{
-            console.log(response)
 
+            /*------------------------------
+            Original Mesh
+            ------------------------------*/
             this.mesh = response.scene.children[0]
+
+            /*------------------------------
+            Material Mesh
+            ------------------------------*/
             this.material = new THREE.MeshBasicMaterial({
                 color:'red',
                 wireframe:true
             })
             this.mesh.material = this.material
 
-            // place on load
+            /*------------------------------
+            Geometry Mesh
+            ------------------------------*/
+            this.geometry = this.mesh.geometry
+            // console.log(this.geometry)
+
+
+            /*------------------------------
+            Particles Material
+            ------------------------------*/
+
+            this.particlesMaterial = new THREE.PointsMaterial({
+                color:'red',
+                size: 0.02
+            })
+
+            /*------------------------------
+            Particles Geometry
+            ------------------------------*/
+
+            const sampler = new MeshSurfaceSampler(this.mesh).build()
+            const numParticles = 20000
+            this.particlesGeometry = new THREE.BufferGeometry()
+            const particlesPosition = new Float32Array(numParticles *3)
+
+            for (let i = 0; i< numParticles;i++){
+                const newPosition = new THREE.Vector3();
+                sampler.sample(newPosition)
+                particlesPosition.set([
+                    newPosition.x, // 0
+                    newPosition.y, // 1
+                    newPosition.z // 2
+                ], i*3 )
+            }
+
+            this.particlesGeometry.setAttribute('position', new
+            THREE.BufferAttribute(particlesPosition,3))
+
+            console.log(this.particlesGeometry)
+
+            /*------------------------------
+            Particles
+            ------------------------------*/
+
+            this.particles = new THREE.Points(this.particlesGeometry,this.particlesMaterial)
+
+            /*------------------------------
+            Place On Load
+            ------------------------------*/
             if(this.placeOnLoad){
                 this.add()
             }
@@ -36,10 +91,10 @@ class Model {
         })
     }
     add(){
-        this.scene.add(this.mesh)
+        this.scene.add(this.particles)
     }
     remove(){
-        this.scene.remove(this.mesh)
+        this.scene.remove(this.particles)
     }
 }
 export default Model;
